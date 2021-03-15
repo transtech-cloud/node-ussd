cf3340c7edacd5a13b6c5faf26ae2effccc80f1b
transtech-cloud <kennedyinfinix@gmail.com>
2021-03-15 20:04:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('./db/mongoose')
+
+
+
+
+require('dotenv').config()
+
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+const port = process.env.PORT || 4000
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
